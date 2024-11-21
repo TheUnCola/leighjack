52c6cfb384a37d3d6901d520fb458ec801db07c2
Steven Sell <Steven.Sell@cscglobal.com>
2024-11-21 18:23:17 -0500

    Added Countdown

--- app/src/app/home/home.component.ts
+++ app/src/app/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {interval, map, Subscription} from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  targetDate = new Date('2024-11-22T00:00:00');
+  countdown: Subscription;
+  countDownTime: any = '';
+
+  ngOnInit() {
+
+    this.countdown = interval(1000).pipe(
+      map(() => {
+        const diff = this.targetDate.getTime() - Date.now();
+        return {
+          days: Math.floor(diff / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+          minutes: Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)),
+          seconds: Math.floor((diff % (1000 * 60)) / 1000)
+        };
+      })
+    ).subscribe(time => {
+      this.countDownTime = time.days + " Days " + time.hours + " Hours " + time.minutes + " Minutes " + time.seconds + " Seconds";
+    });
+  }
+
+  ngOnDestroy() {
+    this.countdown.unsubscribe();
+  }
 }
